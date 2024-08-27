aadbb6826de4552ad6957a98aef5fcf9e3c3b4d6
Marnix Dessing <m.dessing@nijmegen.nl>
2024-08-27 12:32:17 +0200

    chore: add permission boundary aspect to stage

--- src/ApiStage.ts
+++ src/ApiStage.ts
@@ -1,4 +1,5 @@
-import { Stage, StageProps } from 'aws-cdk-lib';
+import { PermissionsBoundaryAspect } from '@gemeentenijmegen/aws-constructs';
+import { Aspects, Stage, StageProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { Configurable } from './Configuration';
 import { UsEastStack } from './UsEastStack';
@@ -9,6 +10,7 @@ export class ApiStage extends Stage {
 
   constructor(scope: Construct, id: string, props: ApiStageProps) {
     super(scope, id, props);
+    Aspects.of(this).add(new PermissionsBoundaryAspect());
 
     /**
      * Some resources must be in us-east-1 for AWS to be able to attach
