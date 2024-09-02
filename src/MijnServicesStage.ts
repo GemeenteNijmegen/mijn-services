7cae60aa849fae39cdc3a48a2c9e1892403c35db
Marnix Dessing <m.dessing@nijmegen.nl>
2024-09-02 22:26:41 +0200

    feat: remove us-east stack as it is not used currently

--- src/MijnServicesStage.ts
+++ src/MijnServicesStage.ts
@@ -5,7 +5,6 @@ import { Configurable } from './Configuration';
 import { DatabaseStack } from './DatabaseStack';
 import { MainStack } from './MainStack';
 import { Statics } from './Statics';
-import { UsEastStack } from './UsEastStack';
 
 interface MijnServicesStageProps extends StageProps, Configurable {}
 
@@ -14,20 +13,6 @@ export class MijnServicesStage extends Stage {
   constructor(scope: Construct, id: string, props: MijnServicesStageProps) {
     super(scope, id, props);
     Aspects.of(this).add(new PermissionsBoundaryAspect());
-
-    /**
-     * Some resources must be in us-east-1 for AWS to be able to attach
-     * them to the CloudFront distribution. This will be the case untill the
-     * AWS European Sovereign Cloud is available and supports CloudFront.
-     * See: https://aws.amazon.com/blogs/security/aws-digital-sovereignty-pledge-announcing-a-new-independent-sovereign-cloud-in-europe/
-     */
-    const usstack = new UsEastStack(this, 'us-east-stack', { // Translates to mijn-services-us-east-stack
-      env: {
-        account: props.configuration.deploymentEnvironment.account,
-        region: 'us-east-1',
-      },
-      configuration: props.configuration,
-    });
 
     /**
      * Creates the database instance (only one currenlty)
@@ -47,7 +32,6 @@ export class MijnServicesStage extends Stage {
       env: props.configuration.deploymentEnvironment,
       configuration: props.configuration,
     });
-    mainstack.addDependency(usstack, 'Cloudfront cert must exist before distribution is created');
     mainstack.addDependency(databasestack, 'Services in main stack need the DB to be created');
 
   }
