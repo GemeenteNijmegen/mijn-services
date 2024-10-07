aad2002d1ddf0002e5d7fd576af403a5cda171ab
Marnix Dessing <m.dessing@nijmegen.nl>
2024-10-07 11:05:40 +0200

    chore: deploy noyify api key for omc

--- src/services/OutputManagementComponent.ts
+++ src/services/OutputManagementComponent.ts
@@ -75,9 +75,8 @@ export class OMCService extends Construct {
       // API keys for ZGW(ish) components
       // USER_API_KEY_OPENKLANT_2: '',
       // USER_API_KEY_OPENKLANT: '', // Cannot be missing and have null or empty value (if you are using OMC Workflow v2 and above; otherwise, it's not mandatory) 	It needs to be generated for OMC Workflow v2 and above from "OpenKlant" 2.0 Web API service UI
-      USER_API_KEY_OBJECTEN: 'NOT IN USE', // Cannot be missing and have null or empty value 	It needs to be generated from "Objecten" Web API service UI
-      USER_API_KEY_OBJECTTYPEN: 'NOT IN USE', // Cannot be missing and have null or empty value 	It needs to be generated from "ObjectTypen" Web API service UI
-      USER_API_KEY_NOTIFYNL: '', // Cannot be missing and have null or empty value + must be in name-UUID-UUID format + must pass Notify NL validation 	It needs to be generated from "Notify NL" Admin Portal
+      // USER_API_KEY_OBJECTEN: 'NOT IN USE', // Cannot be missing and have null or empty value 	It needs to be generated from "Objecten" Web API service UI
+      // USER_API_KEY_OBJECTTYPEN: 'NOT IN USE', // Cannot be missing and have null or empty value 	It needs to be generated from "ObjectTypen" Web API service UI
 
       // Domains for ZGW(ish) components
       USER_DOMAIN_OPENNOTIFICATIES: this.props.omcConfiguration.notificatiesApiUrl, // You have to use ONLY the domain part from URLs where you are hosting the dedicated Open services
@@ -124,6 +123,7 @@ export class OMCService extends Construct {
       // API keys
       USER_API_KEY_OPENKLANT: EcsSecret.fromSecretsManager(this.configurationParameters.openklant),
       USER_API_KEY_OPENKLANT_2: EcsSecret.fromSecretsManager(this.configurationParameters.openklant),
+      USER_API_KEY_NOTIFYNL: EcsSecret.fromSecretsManager(this.configurationParameters.notify),
     };
     return secrets;
   }
