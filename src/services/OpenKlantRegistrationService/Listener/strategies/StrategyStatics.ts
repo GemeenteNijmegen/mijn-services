9c7fa2ed60596c7774d4d2f76f721635114d4b76
Marnix Dessing <m.dessing@nijmegen.nl>
2024-10-21 16:38:47 +0200

    fix: sha265 -> sha256

--- src/services/OpenKlantRegistrationService/Listener/strategies/StrategyStatics.ts
+++ src/services/OpenKlantRegistrationService/Listener/strategies/StrategyStatics.ts
@@ -5,6 +5,6 @@ export class StrategyStatics {
   static readonly PSUEDOID_REGISTER = 'REGISTRATION-SERVICE';
   static constructPseudoId(kvk: string, name: string) {
     const base = `${kvk}-${name}`;
-    return createHash('sha265').update(base).digest('hex');
+    return createHash('sha256').update(base).digest('hex');
   }
 }
