a310a92abfde8deebb0ad8ff95eeb3b0df16b33f
Marnix Dessing <m.dessing@nijmegen.nl>
2025-02-07 16:28:18 +0100

    chore: adapt registration-handler for receiving SQS messages

--- src/services/OpenKlantRegistrationService/RegistrationHandler/registration-handler.lambda.ts
+++ src/services/OpenKlantRegistrationService/RegistrationHandler/registration-handler.lambda.ts
@@ -1,7 +1,5 @@
-import { Response } from '@gemeentenijmegen/apigateway-http';
 import { AWS, environmentVariables } from '@gemeentenijmegen/utils';
-import { APIGatewayProxyEventV2 } from 'aws-lambda';
-import { authenticate } from '../Shared/authenticate';
+import { SQSEvent } from 'aws-lambda';
 import { ErrorResponse } from '../Shared/ErrorResponse';
 import { logger } from '../Shared/Logger';
 import { Notification, NotificationSchema } from '../Shared/model/Notification';
@@ -19,8 +17,6 @@ async function initalize(): Promise<OpenKlantRegistrationServiceProps> {
     'ZGW_TOKEN_CLIENT_ID_ARN',
     'ZGW_TOKEN_CLIENT_SECRET_ARN',
     'ROLTYPES_TO_REGISTER',
-    'HAALCENTRAAL_BRP_APIKEY_ARN',
-    'HAALCENTRAAL_BRP_BASEURL',
   ]);
 
   const [openKlantApiKey, zgwClientId, zgwClientSecret] = await Promise.all([
@@ -50,7 +46,7 @@ async function initalize(): Promise<OpenKlantRegistrationServiceProps> {
 }
 
 let configuration: undefined | OpenKlantRegistrationServiceProps = undefined;
-export async function handler(event: APIGatewayProxyEventV2) {
+export async function handler(event: SQSEvent) {
 
   logger.debug('Incomming event', JSON.stringify(event));
 
@@ -59,40 +55,41 @@ export async function handler(event: APIGatewayProxyEventV2) {
   }
 
   try {
-
-    // Check if the caller send the correct API key
-    // Note: this must be done in the lambda as the API gateway only offers API KEYs for the whole stage.
-    const authenticated = await authenticate(event);
-    if (authenticated !== true) {
-      return authenticated;
-    }
-
     // Create the handler
     const registrationHandler = new OpenKlantRegistrationHandler(configuration);
 
     // Handle the notification event
-    const notification = parseNotificationFromBody(event);
-    return await registrationHandler.handleNotification(notification);
+    const notifications = parseNotificationFromBody(event);
+    for (const notification of notifications) {
+      await registrationHandler.handleNotification(notification);
+    }
 
   } catch (error) {
-    if (error instanceof ErrorResponse) {
-      return Response.error(error.statusCode, error.message);
-    }
-    logger.error('Error during processing of event', error as Error);
-    return Response.error(500);
+    logger.error('Error during processing of notification', error as Error);
+    throw Error('Failed to handle SQS message');
   }
+
 }
 
-function parseNotificationFromBody(event: APIGatewayProxyEventV2): Notification {
+function parseNotificationFromBody(event: SQSEvent): Notification[] {
   try {
-    if (!event.body) {
-      throw Error('Received notification without notification body!');
+
+    if (!event.Records || event.Records.length == 0) {
+      throw Error('Empty event received from queue');
     }
-    let body = event.body;
-    if (event.isBase64Encoded) {
-      body = Buffer.from(event.body, 'base64').toString('utf-8');
+
+    const notifications: Notification[] = [];
+
+    for (const record of event.Records) {
+      if (!record.body) {
+        throw Error('Received notification without notification body!');
+      }
+      const notification = NotificationSchema.parse(JSON.parse(record.body))
+      notifications.push(notification);
     }
-    return NotificationSchema.parse(JSON.parse(body));
+
+    return notifications;
+
   } catch (error) {
     logger.error('Could ont parse notification', error as Error);
     throw new ErrorResponse(400, 'Error parsing body');
