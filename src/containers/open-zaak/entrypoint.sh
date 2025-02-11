6f734b9b72671ec98c1593b646a12ec24d127133
Marnix Dessing <m.dessing@nijmegen.nl>
2025-02-11 21:07:07 +0100

    chore: make sure the configuration docker image can run

--- src/containers/open-zaak/entrypoint.sh
+++ src/containers/open-zaak/entrypoint.sh
@@ -1,8 +1,9 @@
-# Replace environment variables in yaml file and write it to a writable location
-envsubst < setup_configuration.yaml > /app/setup_configuration/data.yaml
+# Replacing environment variables in yaml file and write it to a writable location
+echo "Replacing environment variables in yaml file and write it to a writable location"
+mkdir -p /app/setup_configuration
+envsubst < /setup_configuration.yaml > /app/setup_configuration/data.yaml
+cat /app/setup_configuration/data.yaml
 
 # Call the setup configuration script
+echo "Running setup configuration script from base image"
 /setup_configuration.sh
-
-# Setup kanalen that we are expecting
-python src/manage.py register_kanalen
