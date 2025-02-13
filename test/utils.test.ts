1f8aff5752ca7c703ff83f6749dd8c9ade9283fb
Marnix Dessing <m.dessing@nijmegen.nl>
2025-02-13 12:35:16 +0100

    chore: add test for Utils

---
+++ test/utils.test.ts
@@ -0,0 +1,36 @@
+import { Utils } from '../src/Utils'; // adjust the import path according to your project structure
+
+describe('Utils', () => {
+  describe('toPythonBooleanString', () => {
+
+    it('should throw an error when both arguments are undefined', () => {
+      expect(() => {
+        Utils.toPythonBooleanString();
+      }).toThrow('No value provided and no default value provided');
+    });
+
+    it('should return "True" when the boolean value is true', () => {
+      expect(Utils.toPythonBooleanString(true)).toBe('True');
+    });
+
+    it('should return "False" when the boolean value is false', () => {
+      expect(Utils.toPythonBooleanString(false)).toBe('False');
+    });
+
+    it('should return "True" when boolean is undefined and defaultValue is true', () => {
+      expect(Utils.toPythonBooleanString(undefined, true)).toBe('True');
+    });
+
+    it('should return "False" when boolean is undefined and defaultValue is false', () => {
+      expect(Utils.toPythonBooleanString(undefined, false)).toBe('False');
+    });
+
+    it('should return "False" when boolean is false and defaultValue is undefined', () => {
+      expect(Utils.toPythonBooleanString(false, undefined)).toBe('False');
+    });
+
+    it('should return "True" when boolean is true and defaultValue is undefined', () => {
+      expect(Utils.toPythonBooleanString(true, undefined)).toBe('True');
+    });
+  });
+});
