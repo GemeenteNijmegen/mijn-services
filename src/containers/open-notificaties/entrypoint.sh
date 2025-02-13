c570714c8ba9c1a7a19613c66de2613f07a0229c
Marnix Dessing <m.dessing@nijmegen.nl>
2025-02-13 10:46:24 +0100

    chore: remove cat from entrypoints

--- src/containers/open-notificaties/entrypoint.sh
+++ src/containers/open-notificaties/entrypoint.sh
@@ -2,7 +2,6 @@
 echo "Replacing environment variables in yaml file and write it to a writable location"
 mkdir -p /app/setup_configuration
 envsubst < /setup_configuration.yaml > /app/setup_configuration/data.yaml
-cat /app/setup_configuration/data.yaml
 
 # Call the setup configuration script
 echo "Running setup configuration script from base image"
--- src/containers/open-zaak/entrypoint.sh
+++ src/containers/open-zaak/entrypoint.sh
@@ -5,5 +5,4 @@ envsubst < /setup_configuration.yaml > /app/setup_configuration/data.yaml
 
 # Call the setup configuration script
 echo "Running setup configuration script from base image"
-# cat /app/setup_configuration/data.yaml
 /setup_configuration.sh
