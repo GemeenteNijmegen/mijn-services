453a5a47ad85bfdb00f7590346571b5dc6511cb8
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2025-04-24 09:41:26 +0200

    chore: fix home dir

--- src/TransferUser.ts
+++ src/TransferUser.ts
@@ -2,21 +2,38 @@ import { FileSystem } from 'aws-cdk-lib/aws-efs';
 import { Effect, Policy, PolicyStatement, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
 import { CfnUser } from 'aws-cdk-lib/aws-transfer';
 import { Construct } from 'constructs';
+import path from 'path';
 import { TransferServer } from './TransferServer';
 
 interface TransferUserProps {
   server: TransferServer;
   filesystem: FileSystem;
+
+  /** Home directory for the user
+   *  For EFS: Prepended with /<filesystemid>
+   * 
+   * Default: EFS filesystem root 
+   */
+  homeDirectory?: string;
 }
 
 export class TransferUser extends Construct {
   constructor(scope: Construct, id: string, props: TransferUserProps) {
     super(scope, id);
+    const homeDirectory = this.homeDirectory(props);
+
     new CfnUser(this, 'user', {
       role: this.role(props.filesystem).roleArn,
       serverId: props.server.serverId(),
       userName: 'sftpuser',
+      homeDirectory
     });
+  }
+
+  private homeDirectory(props: TransferUserProps) {
+    return props.homeDirectory
+      ? path.join('/', props.filesystem.fileSystemId, props.homeDirectory)
+      : `/${props.filesystem.fileSystemId}`;
   }
 
   role(filesystem: FileSystem) {
