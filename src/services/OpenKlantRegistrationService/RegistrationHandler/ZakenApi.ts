2dcd428b8ff6dac4bdf7caf6448e071a660bd4fd
Marnix Dessing <m.dessing@nijmegen.nl>
2025-06-16 19:59:14 +0200

    fix: contactpersoonRol name field can be empty string

--- src/services/OpenKlantRegistrationService/RegistrationHandler/ZakenApi.ts
+++ src/services/OpenKlantRegistrationService/RegistrationHandler/ZakenApi.ts
@@ -1,7 +1,7 @@
-import { NotFoundError, ZgwApi, ZgwApiProps } from './ZgwApi';
 import { logger } from '../Shared/Logger';
 import { Rol, RolSchema } from '../Shared/model/Rol';
 import { Zaak, ZaakSchema } from '../Shared/model/Zaak';
+import { NotFoundError, ZgwApi, ZgwApiProps } from './ZgwApi';
 
 export interface IZakenApi {
   getRol(url: string): Promise<Rol>;
@@ -43,6 +43,10 @@ export class ZakenApi extends ZgwApi implements IZakenApi {
   async updateRol(rol: Rol): Promise<Rol> {
     if (!rol.url) {
       throw Error('Cannot update a rol without URL');
+    }
+
+    if (!rol.contactpersoonRol?.naam || rol.contactpersoonRol.naam === "") {
+      throw Error('Cannot update a rol without naam in contactpersoonRol');
     }
 
     const originalRol = await this.getRol(rol.url);
