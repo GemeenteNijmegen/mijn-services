41c7ddd00d1162e57ed12e41a0c05b2b8ece579f
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2025-07-02 14:17:28 +0200

    chore: fix id

--- src/constructs/LoadBalancer.ts
+++ src/constructs/LoadBalancer.ts
@@ -39,7 +39,7 @@ export class LoadBalancer extends Construct {
     return httpsListener;
   }
 
-  attachECSService(service: FargateService, domain: string, priority?: number) {
+  attachECSService(id: string, service: FargateService, domain: string, priority?: number) {
     const listenerProps = {
       port: 80,
       targets: [service],
@@ -60,7 +60,7 @@ export class LoadBalancer extends Construct {
       deregistrationDelay: Duration.minutes(1),
     };
     console.debug(listenerProps);
-    this.listener.addTargets(`${service.node.id}-ecs-target`, listenerProps);
+    this.listener.addTargets(id, listenerProps);
     this.priority += 1;
   }
 }
