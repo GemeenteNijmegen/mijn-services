ac27b758d13110270494930d42be4375adc1ea83
Marnix Dessing <m.dessing@nijmegen.nl>
2025-07-29 14:48:56 +0200

    chore: make loadbalancer listen to original path header

--- src/constructs/LoadBalancer.ts
+++ src/constructs/LoadBalancer.ts
@@ -1,7 +1,7 @@
-import { StackProps, Duration } from 'aws-cdk-lib';
+import { Duration, StackProps } from 'aws-cdk-lib';
 import { IVpc, SubnetType } from 'aws-cdk-lib/aws-ec2';
 import { FargateService } from 'aws-cdk-lib/aws-ecs';
-import { IListenerCertificate, ApplicationLoadBalancer, ApplicationListener, ListenerAction, ListenerCondition, Protocol, AddApplicationTargetsProps } from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import { AddApplicationTargetsProps, ApplicationListener, ApplicationLoadBalancer, IListenerCertificate, ListenerAction, ListenerCondition, Protocol } from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 
@@ -54,7 +54,7 @@ export class ServiceLoadBalancer extends Construct {
       port: 80,
       targets: [service],
       conditions: [
-        ListenerCondition.pathPatterns([path]),
+        ListenerCondition.httpHeader('x-original-uri', [path]), // Set by cloudfront function (used for path rewrites)
       ],
       priority: priority ?? this.priority,
       healthCheck: {
