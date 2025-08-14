9cde1131083e5cf3510240bb7e7189b2ade34c2c
WietekeNijmegen <wietekenijmegen@gmail.com>
2025-08-14 15:55:35 +0200

    chore: nginx

--- src/services/GZACFrontend.ts
+++ src/services/GZACFrontend.ts
@@ -72,10 +72,7 @@ export class GZACFrontendService extends Construct {
     // Main service container
     // const container =
     task.addContainer('gzac-frontend', {
-      // image: ContainerImage.fromAsset('./src/containers/gzac-frontend'),
-      image: ContainerImage.fromRegistry(
-        this.props.serviceConfiguration.image,
-      ),
+      image: ContainerImage.fromAsset('./src/containers/gzac-frontend'),
       healthCheck: {
         command: ['CMD-SHELL', 'exit 0'],
         interval: Duration.seconds(10),
