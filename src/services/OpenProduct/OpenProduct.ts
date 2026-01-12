5a8ccbb4142b29d049ec5771509e216b00bebbaf
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2026-01-12 11:46:04 +0100

    feat: Allow open product to read bucket contents

--- src/services/OpenProduct/OpenProduct.ts
+++ src/services/OpenProduct/OpenProduct.ts
@@ -186,6 +186,7 @@ export class OpenProductService extends Construct {
       },
     });
 
+    this.props.openConfigStore.grantReadConfig(service.taskDefinition.executionRole!, 'open-product');
     this.setupConnectivity('main', service.connections.securityGroups);
     this.allowAccessToSecrets(service.taskDefinition.executionRole!);
     return service;
