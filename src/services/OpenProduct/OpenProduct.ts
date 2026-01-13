54d204e66ecde982655c1d3e829bf1df48ec2318
Marnix Dessing <m.dessing@nijmegen.nl>
2026-01-13 09:31:41 +0100

    chore: remove open-product port

--- src/services/OpenProduct/OpenProduct.ts
+++ src/services/OpenProduct/OpenProduct.ts
@@ -72,7 +72,7 @@ export class OpenProductService extends Construct {
       CACHE_AXES: cacheHost + this.props.cacheDatabaseIndex,
       SUBPATH: '/' + this.props.path,
       IS_HTTPS: 'True',
-      UWSGI_PORT: this.props.service.port.toString(),
+      // UWSGI_PORT: this.props.service.port.toString(), // MD: this breakes UWSGI aparently now?
 
       LOG_LEVEL: this.props.openProductConfiguration.logLevel,
       LOG_REQUESTS: Utils.toPythonBooleanString(this.props.openProductConfiguration.debug, false),
