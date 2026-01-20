4bb859c2463e86c9936e74b0448b98fd5b8e0141
Marnix Dessing <m.dessing@nijmegen.nl>
2026-01-20 12:22:01 +0100

    chore: setup env variables arc lambda

--- src/services/AttestatieRegistratieComponent/AttestatieRegistratieComponent.ts
+++ src/services/AttestatieRegistratieComponent/AttestatieRegistratieComponent.ts
@@ -1,4 +1,5 @@
 import { Function } from 'aws-cdk-lib/aws-lambda';
+import { Secret } from 'aws-cdk-lib/aws-secretsmanager';
 import { Construct } from 'constructs';
 import { ArcFunction } from './lambda/arc-function';
 import { ServiceLoadBalancer } from '../../constructs/LoadBalancer';
@@ -15,7 +16,21 @@ export class AttestatieRegistratieComponent extends Construct {
   }
 
   private setupLambda() {
-    const arc = new ArcFunction(this, 'arc-function', {});
+
+    const clientSecret = new Secret(this, 'verid-client-secret', {
+      description: 'Client secret VerID issueance',
+    });
+
+    const arc = new ArcFunction(this, 'arc-function', {
+      environment: {
+        VERID_CLIENT_ID: '6828f0a8-1c4c-478b-b60e-3db863a8a42e',
+        VERID_CLIENT_SECRET: clientSecret.secretArn,
+        VERID_ISSUER_URL: 'https://oauth.ssi.dev.ver.garden',
+        ARC_CALLBACK_ENDPOINT: 'https://mijn-services-dev.csp-nijmegen.nl/arc/callback',
+      },
+    });
+
+    clientSecret.grantRead(arc);
     this.setupRoute(arc);
   }
 
