ae8879339e5a5cee8a9543e09db3030d6bbc8e86
Marnix Dessing <m.dessing@nijmegen.nl>
2026-03-25 16:14:02 +0100

    chore: disable in container health check

--- src/services/VTB.ts
+++ src/services/VTB.ts
@@ -130,7 +130,8 @@ export class VtbService extends Construct {
     const container = task.addContainer('main', {
       image: ContainerImage.fromRegistry(this.props.serviceConfiguration.image),
       healthCheck: {
-        command: ['CMD-SHELL', `python -c "import requests; x = requests.get('http://localhost:${this.props.service.port}/'); exit(x.status_code != 200)" >> /proc/1/fd/1`],
+        // command: ['CMD-SHELL', `python -c "import requests; x = requests.get('http://localhost:${this.props.service.port}/'); exit(x.status_code != 200)" >> /proc/1/fd/1`],
+        command: ['CMD-SHELL', `exit 0`],
         interval: Duration.seconds(10),
         startPeriod: Duration.seconds(30),
       },
