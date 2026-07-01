030ad33b03e94d697a12d4e675c2239e663d2fff
Marnix Dessing <m.dessing@nijmegen.nl>
2026-07-01 11:09:08 +0200

    chore: fix health checks

--- src/services/OpenZaakv2.ts
+++ src/services/OpenZaakv2.ts
@@ -178,7 +178,7 @@ export class OpenZaakv2Service extends Construct {
       conditions: [ListenerCondition.hostHeaders([fqdomain])],
       healthCheck: {
         enabled: true,
-        path: '/health/ready',
+        path: '/',
         healthyHttpCodes: '200',
         healthyThresholdCount: 2,
         unhealthyThresholdCount: 6,
