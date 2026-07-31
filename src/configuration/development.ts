4b1958e459d0209253f07bac31a8d0388a6c1d96
Marnix Dessing <m.dessing@nijmegen.nl>
2026-07-31 11:32:10 +0200

    chore: info loggin for keycloak

--- src/configuration/development.ts
+++ src/configuration/development.ts
@@ -160,7 +160,7 @@ export const development: Configuration = {
       databaseName: 'mijn-services-keycloak',
       id: 'mijn-services-keycloak',
       image: 'quay.io/keycloak/keycloak:26.7.0',
-      logLevel: 'DEBUG',
+      logLevel: 'INFO',
       subdomain: 'keycloak',
       debug: true,
       loadbalancerPriority: 50,
